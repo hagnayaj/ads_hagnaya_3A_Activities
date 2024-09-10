9165c626fd3e2fb7f18dd60e22bd8c4c456c66f0
adshagnaya <johnlloydhagnaya24@gmail.com>
2024-09-09 23:27:15 -0700

    Complted Activity 3: Eloquent ORM Basics

--- routes/web.php
+++ routes/web.php
@@ -19,22 +19,29 @@ Route::get('/student', function () {
 
 Route::get('/student/update', function () {
     $student = student::where('email', 'johndoe@example.com')->first();
-    $student->email = 'john.doe@newmail.com'
-    
-})
+    $student->email = 'john.doe@newmail.com';
+    $student->age = 23;
+    $student->save();
+    return 'Student Updated!';   
+});
 
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::get('students/delete', function(){
+    $student = student::where('email', 'john.doe@newmail.com')->first();
+    $student->delete();
+    return 'Student Deleted!';
+});
 
+Route::get('/course/create', function(){
+    $course = new Course();
+    $course->course_name = 'Introduction to Database';
+    $course->save();
+    return 'Course Created!';
+});
 Route::get('/', function () {
     return view('welcome');
+});
+Route::get('/course/{id}/students', function($id){
+    $course = Course::find($id);
+    return $course-.students;
 });
